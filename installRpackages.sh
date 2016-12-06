c41a8cc8f8cea22a1032418fbda0f50167481eb7
mjmg <mjmg@users.noreply.github.com>
2016-12-06 22:45:22 +0800

    Create installRpackages.sh

---
+++ installRpackages.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# This installs R packages in CRAN
+echo "Installing ggplot2 from CRAN"
+Rscript -e "install.packages('ggplot2')"
+
+# This installs R packages from github
+echo "Installing hadley/dplyr from github"
+Rscript -e "library(devtools); install_github('dplyr', 'hadley')"
+
+# This installs opencpu webapps from github
+echo "Installing appdemo, gitstats, tvscore and qitools/charts opencpu webapp"
+Rscript -e "library(devtools); install_github('mjmg/appdemo')"
+Rscript -e "library(devtools); install_github('mjmg/gitstats')"
+Rscript -e "library(devtools); install_github('mjmg/tvscore')"
+Rscript -e "library(devtools); install_github('qitools/charts')"
+
+
+# This installs R packages under Bioconductor
+echo "Installing EBImage from Bioconductor"
+Rscript -e "source('https://bioconductor.org/biocLite.R'); biocLite('BiocStyle')"
+Rscript -e "source('https://bioconductor.org/biocLite.R'); biocLite('EBImage')"
